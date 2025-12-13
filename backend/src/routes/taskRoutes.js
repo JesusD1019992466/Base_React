75dc3330068285d800d72bd75776dd2988be1084
JesúsD1019992466 <jesusd_riverar@soy.sena.edu.co>
2025-12-12 20:32:32 -0500

    Agrega endpoint de prueba /api/tasks/ping

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -9,5 +9,8 @@ router.get('/:id', TaskController.getTaskById);
 router.post('/', TaskController.createTask);
 router.put('/:id', TaskController.updateTask);
 router.delete('/:id', TaskController.deleteTask);
+router.get('/ping', (req, res) => {
+  res.json({ message: 'Ruta /api/tasks/ping funcionando' });
+});
 
 export default router;
